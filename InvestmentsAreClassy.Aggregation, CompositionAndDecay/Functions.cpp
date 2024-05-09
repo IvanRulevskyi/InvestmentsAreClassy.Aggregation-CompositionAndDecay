38c18f5887f40ab365d9339025c75df92bd71684
IvanRulevskyi <rulevskiy4@gmail.com>
2024-05-09 15:59:08 +0300

    Finish

---
+++ InvestmentsAreClassy.Aggregation, CompositionAndDecay/Functions.cpp
@@ -0,0 +1,8 @@
+//
+//  Functions.cpp
+//  InvestmentsAreClassy.Aggregation, CompositionAndDecay
+//
+//  Created by Иван Рулевский on 09.05.2024.
+//
+
+#include "Functions.hpp"
---
+++ InvestmentsAreClassy.Aggregation, CompositionAndDecay/Functions.hpp
@@ -0,0 +1,13 @@
+//
+//  Functions.hpp
+//  InvestmentsAreClassy.Aggregation, CompositionAndDecay
+//
+//  Created by Иван Рулевский on 09.05.2024.
+//
+
+#ifndef Functions_hpp
+#define Functions_hpp
+
+#include <stdio.h>
+
+#endif /* Functions_hpp */
